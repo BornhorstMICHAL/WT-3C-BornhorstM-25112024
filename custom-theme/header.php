d63b20bd9ab5f160bde03269e1cce5ed52ef279e
BornhorstMICHAL <144802672+BornhorstMICHAL@users.noreply.github.com>
2024-11-25 15:16:07 +0100

    Delete custom-theme directory

--- custom-theme/header.php
+++
@@ -1,5 +0,0 @@
-<nav style="width: 100%; background-color: wheat;">
-    <a style="height: 40px;" href="">Link1</a>
-    <a style="height: 40px;" href="">Link2</a>
-    <a style="height: 40px;" href="">Link3</a>
-</nav>
