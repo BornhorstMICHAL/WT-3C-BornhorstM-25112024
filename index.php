c0e7fae46e4a292154e5d33619512e654c9e5e63
BornhorstMICHAL <144802672+BornhorstMICHAL@users.noreply.github.com>
2024-12-09 09:00:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,32 @@ else :
 endif;
 ?>
 
+<?php the_category(); ?>  
 <h3><?php the_title(); ?></h3>
 
-<?php the_content(); ?>
-<?php wp_link_pages(); ?>
+<div class="two-column-layout">
+    <div class="column-left">
+        <!-- Levý sloupec -->
+        <?php the_author(); ?>
+        <?php if (has_post_thumbnail()) : ?>
+            <div class="post-thumbnail">
+                <?php the_post_thumbnail('large'); ?>
+            </div>
+        <?php endif; ?>
+    </div>
+    <div class="column-right">
+        <!-- Pravý sloupec -->
+        <h1 class="post-title"><?php the_title(); ?></h1>
+        <strong><?php the_excerpt() ?></strong>
+        <div class="post-content">
+            <?php the_content(); ?>
+            <?php wp_link_pages(); ?>
+        </div>
+    </div>
+</div>
+
 <?php edit_post_link(); ?>
+
 
 <?php endwhile; ?>
 
